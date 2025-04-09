f9746966af7251275b3857b6c85286525c8748da
subhranshudev <subhranshusekhar2509@gmail.com>
2025-04-09 08:07:46 +0530

    Implement user registration API and database connection logic

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,41 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
-    throw new Error("Please define mongodb uri in env file")
+  throw new Error("Please define mongodb uri in env file");
+}
+console.log("GLOBAL: ", global);
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
-let cached = global.mongoose
+export async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: true,
+      maxPoolSize: 10,
+    };
+    
+    cached.promise = mongoose
+    .connect(MONGODB_URI, opts)
+    .then(() => mongoose.connection);
+}
+
+try {
+   cached.conn = await cached.promise 
+} catch (error) {
+    cached.promise = null
+    throw error
+}
+
+return cached.conn;
+
+}
